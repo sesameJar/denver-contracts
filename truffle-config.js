521fd4bc4819a4f5412d8acc1e3c936e1245edd1
ehsueh <emma.yc.hsueh@gmail.com>
2021-02-09 11:47:23 -0500

    add deploy config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const INFURA = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const MNEUMONICS = process.env.MNEUMONICS;
 module.exports = {
   // Uncommenting the defaults below 
   // provides for an easier quick-start with Ganache.
@@ -21,6 +25,16 @@ module.exports = {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*"
+        },
+        rinkeby: {
+            provider: function () {
+                return new HDWalletProvider(MNEUMONICS, INFURA);
+            },
+            // provider: new LedgerWalletProvider(ledgerOptions, INFURA),
+            network_id: 4,
+            gas: 6000000,
+            gasPrice: 25000000000, // 25 Gwei. default = 100 gwei = 100000000000
+            skipDryRun: true
         },
     }
   
